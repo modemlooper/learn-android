647c5d26397ad3af049cc5a139b32b6303dc749f
modemlooper <modemlooper@gmail.com>
2025-10-19 09:43:50 -0700

    bottom sheet

--- app/src/main/java/com/palmersquare/myapplication/screens/Home.kt
+++ app/src/main/java/com/palmersquare/myapplication/screens/Home.kt
@@ -1,21 +1,35 @@
 package com.palmersquare.myapplication.screens
 
+import androidx.activity.result.launch
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.view.isVisible
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 import com.palmersquare.myapplication.Screen
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,6 +37,11 @@ import java.util.UUID
 fun Home(
     homeBackStack: NavBackStack<NavKey>
 ) {
+    // 1. State for managing the bottom sheet's visibility and behavior
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -42,10 +61,46 @@ fun Home(
                 .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Button(onClick = {
-                homeBackStack.add(Screen.Detail(UUID.randomUUID().toString()))
-            }) {
-                Text(text = "Go to Detail")
+
+            Column {
+                Button(onClick = {
+                    homeBackStack.add(Screen.Detail(UUID.randomUUID().toString()))
+                }) {
+                    Text(text = "Go to Detail")
+                }
+
+                Button(onClick = {
+                    showBottomSheet = true
+                }) {
+                    Text(text = "Open Sheet")
+                }
+            }
+        }
+    }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            },
+            sheetState = sheetState
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(all = 16.dp)
+                    .fillMaxHeight(0.9f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(onClick = {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showBottomSheet = false
+                        }
+                    }
+                }) {
+                    Text("Click to close")
+                }
             }
         }
     }
